964a391717727408c786a4390ae4fa89b7dda42a
kaknazlo <kaknazlo@gmail.com>
2019-12-23 11:43:10 +0200

    add file

---
+++ ssdd/js/main.js
@@ -0,0 +1,37 @@
+ function ss1() {
+     document.querySelector(".big").setAttribute("style", "transform: rotate3d(0, 0, 9, 47deg); top: 30px; left: 168px;transition-duration: 2s, 5s, 2s;transition-delay: 0s, 0s, 2s;");
+     console.log('dd')
+ }
+
+ function year_2014() {
+     document.querySelector(".big").setAttribute("style", "transform: rotate3d(0, 0, 9, 47deg); top: 99px; left: 205px;transition-duration: 2s, 5s, 2s;transition-delay: 0s, 0s, 2s;");
+     console.log('dd')
+ }
+
+ function year_2015() {
+     document.querySelector(".big").setAttribute("style", "transform: rotate3d(0, 0, 9, 57deg); top: 175px; left: 180px;transition-duration: 2s, 5s, 2s;transition-delay: 0s, 0s, 2s;");
+     console.log('dd')
+ }
+
+ function year_2016() {
+     document.querySelector(".big").setAttribute("style", "transform: rotate3d(0, 0, 9, 27deg); top: 205px; left: 110px;transition-duration: 2s, 5s, 2s;transition-delay: 0s, 0s, 2s;");
+     console.log('ss')
+ }
+
+ function year_2017() {
+     document.querySelector(".big").setAttribute("style", "transform: rotate3d(0, 0, 9, 47deg); top: 160px; left: 18px;transition-duration: 2s, 5s, 2s;transition-delay: 0s, 0s, 2s;");
+     console.log('dd')
+ }
+
+
+ function year_2018() {
+     document.querySelector(".big").setAttribute("style", "    transform: translateX(-13px) rotate(33deg) translateY(-30px); top: 115px; left: 5px;transition-duration: 2s, 5s, 2s;transition-delay: 0s, 0s, 2s;");
+     document.querySelector(".small").setAttribute("style", "    transform:  rotate(33deg);transition-duration: 1s, 5s, 2s;transition-delay: 0s, 0s, 2s;");
+
+     console.log('dd')
+ }
+
+ function year_2019() {
+     document.querySelector(".big").setAttribute("style", "transform: translateX(3px) rotate(34deg) translateY(-9px); top: 35px; left: 35px;transition-duration: 2s, 5s, 2s;transition-delay: 0s, 0s, 2s;");
+     console.log('dd')
+ }
