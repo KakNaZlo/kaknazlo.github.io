fc590f032cef98be0d53853bd4b73d9f5b75df2e
kaknazlo <kaknazlo@gmail.com>
2019-12-26 18:57:33 +0200

    add fule

--- ssdd/js/main.js
+++ ssdd/js/main.js
@@ -1,37 +1,159 @@
- function ss1() {
-     document.querySelector(".big").setAttribute("style", "transform: rotate3d(0, 0, 9, 47deg); top: 30px; left: 168px;transition-duration: 2s, 5s, 2s;transition-delay: 0s, 0s, 2s;");
-     console.log('dd')
- }
+function rotateTo(deg) {
 
- function year_2014() {
-     document.querySelector(".big").setAttribute("style", "transform: rotate3d(0, 0, 9, 47deg); top: 99px; left: 205px;transition-duration: 2s, 5s, 2s;transition-delay: 0s, 0s, 2s;");
-     console.log('dd')
- }
+    var bplate = [
+    "translate(-50%,-50%) rotate(", -90, "deg) translateY(-100px) rotate(", 190, "deg)"
+  ];
+    bplate[1] = deg;
+    bplate[3] = deg;
 
- function year_2015() {
-     document.querySelector(".big").setAttribute("style", "transform: rotate3d(0, 0, 9, 57deg); top: 175px; left: 180px;transition-duration: 2s, 5s, 2s;transition-delay: 0s, 0s, 2s;");
-     console.log('dd')
- }
+    let roundSmall = [
+        " rotate(", 190, "deg)"
+    ];
+    roundSmall[1] = deg;
+    let roundMain = [
+        "rotate(", -720, "deg)"
+    ];
+    roundMain[1] = deg;
+    $(".big").css({
+        "transform": bplate.join("")
+    });
+    $(".small ").css({
+        "transform": roundSmall.join("")
+    });
+    $(".main-circle ").css({
+        "transform": roundSmall.join("")
+    });
 
- function year_2016() {
-     document.querySelector(".big").setAttribute("style", "transform: rotate3d(0, 0, 9, 27deg); top: 205px; left: 110px;transition-duration: 2s, 5s, 2s;transition-delay: 0s, 0s, 2s;");
-     console.log('ss')
- }
 
- function year_2017() {
-     document.querySelector(".big").setAttribute("style", "transform: rotate3d(0, 0, 9, 47deg); top: 160px; left: 18px;transition-duration: 2s, 5s, 2s;transition-delay: 0s, 0s, 2s;");
-     console.log('dd')
- }
+}
+$(".circle-block").click(function (e) {
+
+    var deg = $(e.target).html()
 
 
- function year_2018() {
-     document.querySelector(".big").setAttribute("style", "    transform: translateX(-13px) rotate(33deg) translateY(-30px); top: 115px; left: 5px;transition-duration: 2s, 5s, 2s;transition-delay: 0s, 0s, 2s;");
-     document.querySelector(".small").setAttribute("style", "    transform:  rotate(33deg);transition-duration: 1s, 5s, 2s;transition-delay: 0s, 0s, 2s;");
+    deg = deg === "1" ? 30 :
+        deg === "2014" ? 90 :
+        deg === "2015" ? 130 :
+        deg === "2016" ? 180 :
+        deg === "2017" ? 235 :
+        deg === "2018" ? 260 :
+        deg === "2019" ? 308 : -30;
+    rotateTo(deg);
+});
 
-     console.log('dd')
- }
 
- function year_2019() {
-     document.querySelector(".big").setAttribute("style", "transform: translateX(3px) rotate(34deg) translateY(-9px); top: 35px; left: 35px;transition-duration: 2s, 5s, 2s;transition-delay: 0s, 0s, 2s;");
-     console.log('dd')
- }
+//popup
+// form
+
+$('.mess-item').click(function () {
+    $('.overlay-form').fadeIn();
+
+});
+
+// Закрытие окна
+$(document).mouseup(function (e) {
+    var pop = $('.pop_form');
+    if (e.target != pop[0] && pop.has(e.target).length === 0) {
+        $('.overlay-form').fadeOut();
+
+    }
+});
+
+// year-2014
+
+$('.year-2014').click(function () {
+    $('.overlay4').fadeIn();
+    $('.year-2014').addClass("year-right-active");
+});
+
+// Закрытие окна
+$(document).mouseup(function (e) {
+    var pop = $('.pop_2014');
+    if (e.target != pop[0] && pop.has(e.target).length === 0) {
+        $('.overlay4').fadeOut();
+        $('.year-2014').removeClass("year-right-active");
+    }
+});
+
+// year-2015
+
+$('.year-2015').click(function () {
+    $('.overlay5').fadeIn();
+    $('.year-2015').addClass("year-right-active");
+});
+
+// Закрытие окна
+$(document).mouseup(function (e) {
+    var pop = $('.pop_2015');
+    if (e.target != pop[0] && pop.has(e.target).length === 0) {
+        $('.overlay5').fadeOut();
+        $('.year-2015').removeClass("year-right-active");
+    }
+});
+
+// year-2016
+
+$('.year-2016').click(function () {
+    $('.overlay6').fadeIn();
+    $('.year-2016').addClass("year-2016-active");
+});
+
+// Закрытие окна
+$(document).mouseup(function (e) {
+    var pop = $('.pop_2016');
+    if (e.target != pop[0] && pop.has(e.target).length === 0) {
+        $('.overlay6').fadeOut();
+        $('.year-2016').removeClass("year-2016-active");
+    }
+});
+
+// year-2017
+
+$('.year-2017').click(function () {
+    $('.overlay7').fadeIn();
+    $('.year-2017').addClass("year-2017-active");
+
+});
+
+// Закрытие окна
+$(document).mouseup(function (e) {
+    var pop = $('.pop_2017');
+    if (e.target != pop[0] && pop.has(e.target).length === 0) {
+        $('.overlay7').fadeOut();
+        $('.year-2017').removeClass("year-2017-active");
+    }
+});
+
+
+// year-2018
+
+$('.year-2018').click(function () {
+    $('.overlay8').fadeIn();
+    $('.year-2018').addClass("year-left-active");
+});
+
+// Закрытие окна
+$(document).mouseup(function (e) {
+    var pop = $('.pop_2018');
+    if (e.target != pop[0] && pop.has(e.target).length === 0) {
+        $('.overlay8').fadeOut();
+        $('.year-2018').removeClass("year-left-active");
+    }
+});
+
+// year-2019
+
+$('.year-2019').click(function () {
+    $('.overlay9').fadeIn();
+    $('.year-2019').addClass("year-left-active");
+
+});
+
+// Закрытие окна
+$(document).mouseup(function (e) {
+    var pop = $('.pop_2019');
+    if (e.target != pop[0] && pop.has(e.target).length === 0) {
+        $('.overlay9').fadeOut();
+        $('.year-2019').removeClass("year-left-active");
+    }
+});
