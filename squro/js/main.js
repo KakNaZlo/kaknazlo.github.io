40fbeb8c1595d2fdf439458cc22c805bd2488151
kaknazlo <kaknazlo@gmail.com>
2020-05-07 19:19:44 +0300

    add file

---
+++ squro/js/main.js
@@ -0,0 +1,175 @@
+//popup
+
+$('.form_btn').click( function() {
+    $('.popup_overlay').fadeIn();
+});
+$(document).mouseup( function (e) {
+    var popup = $('.popup');
+    if (e.target != popup[0] && popup.has(e.target).length === 0){
+        $('.popup_overlay').fadeOut();
+    }
+});
+
+
+
+
+
+
+
+jQuery(document).ready(function($) {
+    $('.slide_block').slick({
+        dots: true,
+        appendDots:$('.slide-dots'),
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2500,
+        arrows: false,
+        responsive: [{
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+            {
+                breakpoint: 400,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }]
+    });
+
+
+
+    // const menuBtn = $('.nav-mobile'),
+    //     menu    = $('.nav_content');
+    //
+    // menuBtn.on('click', function() {
+    //     if ( $(this).hasClass('is-active') ) {
+    //         $(this).removeClass('is-active');
+    //         menu.slideUp();
+    //     } else {
+    //         $(this).addClass('is-active');
+    //         menu.slideDown();
+    //     }
+    // });
+    //
+    // $(document).click(function (e) {
+    //     if ( !menuBtn.is(e.target) && !menu.is(e.target) && menu.has(e.target).length === 0) {
+    //         menu.slideUp();
+    //         menuBtn.removeClass('is-active');
+    //     };
+    // });
+    // $(document).keydown(function(eventObject){
+    //     if ( eventObject.which == 27 ) {
+    //         menu.slideUp();
+    //         menuBtn.removeClass('is-active');
+    //     };
+    // });
+});
+
+function selectCountry() {
+    var select = document.querySelector("#selectCountry");
+    var options = ['Country', 'Afghanistan', 'Albania', 'Algeria', 'American Samoa', 'Andorra', 'Angola', 'Anguilla', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bangladesh', 'Barbados', 'Bahamas', 'Bahrain', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'British Indian Ocean Territory', 'British Virgin Islands', 'Brunei Darussalam', 'Bulgaria', 'Burkina Faso', 'Burma', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Cayman Islands', 'Central African Republic', 'Chad', 'Chile', 'China', 'Christmas Island', 'Cocos (Keeling) Islands', 'Colombia', 'Comoros', 'Congo-Brazzaville', 'Congo-Kinshasa', 'Cook Islands', 'Costa Rica', 'Croatia', 'Cura?ao', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'East Timor', 'Ecuador', 'El Salvador', 'Egypt', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia', 'Falkland Islands', 'Faroe Islands', 'Federated States of Micronesia', 'Fiji', 'Finland', 'France', 'French Guiana', 'French Polynesia', 'French Southern Lands', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guadeloupe', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Heard and McDonald Islands', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macau', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Martinique', 'Mauritania', 'Mauritius', 'Mayotte', 'Mexico', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Niue', 'Norfolk Island', 'Northern Mariana Islands', 'Norway', 'Oman', 'Pakistan', 'Palau', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Pitcairn Islands', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'R?union', 'Romania', 'Russia', 'Rwanda', 'Saint Barth?lemy', 'Saint Helena', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Martin', 'Saint Pierre and Miquelon', 'Saint Vincent', 'Samoa', 'San Marino', 'S?o Tom? and Pr?ncipe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Sint Maarten', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'South Georgia', 'South Korea', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'Svalbard and Jan Mayen', 'Sweden', 'Swaziland', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', 'Togo', 'Tokelau', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Turks and Caicos Islands', 'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Vatican City', 'Vietnam', 'Venezuela', 'Wallis and Futuna', 'Western Sahara', 'Yemen', 'Zambia', 'Zimbabwe'];
+    for (var i = 0; i < options.length; i++) {
+        var opt = options[i];
+        var el = document.createElement("option");
+        el.textContent = opt;
+        el.value = opt;
+        select.appendChild(el);
+        select.value = ("Country");
+    }
+}
+
+selectCountry();
+
+var input = document.querySelector(".phone_input");
+window.intlTelInput(input, {
+
+    utilsScript: "js/utils.js",
+});
+
+$(document).ready( function() {
+    $("#fl_inp").change(function(){
+        var filename = $(this).val().replace(/.*\\/, "");
+        $(".file_name").html(filename);
+    });
+});
+
+$(function() {
+    $( ".datepicker" ).datepicker();
+});
+
+
+$(function() {
+
+    var ready = true;
+
+    $('.tooltip').click(function(){
+
+        if (ready) {
+
+            ready = false;
+
+            var textBlockClass = $(this).data('ref');
+
+            if (!$(this).hasClass('active')) { // showing
+
+                $('.tooltip.active').removeClass('active');
+                $(this).addClass('active');
+
+
+                // hiding currently visible text and showing needed text
+                $('.tooltip_info.active').not('.'+textBlockClass).removeClass('active');
+                $('.tooltip_info.'+textBlockClass).addClass('active');
+
+            } else { // returning to base state
+
+                $(this).removeClass('active');
+                $('.tooltip_info.active').removeClass('active');
+
+
+            }
+
+
+            ready = true;
+        }
+    });
+
+});
+
+
+(function($) {
+    $(function() {
+        $('nav ul li a:not(:only-child)').click(function(e) {
+            $(this).siblings('.sub_menu').toggle();
+            $('.dropdown').not($(this).siblings()).hide();
+            e.stopPropagation();
+        });
+        $('html').click(function() {
+            $('.sub_menu').hide();
+        });
+
+
+
+        $('#nav-toggle').click(function() {
+            $('.mobile_menu').toggle();
+        });
+        $('#nav-toggle').on('click', function() {
+            this.classList.toggle('active');
+        });
+        $(document).keydown(function(eventObject){
+            if ( eventObject.which == 27 ) {
+                $('.mobile_menu').slideToggle();
+                $('#nav-toggle').removeClass('active');
+
+            };
+        });
+
+    });
+})(jQuery);
